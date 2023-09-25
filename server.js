d7cd16ff1f69a4444f66ec0cf6cf5a3e2bd6d36b
AdrianResende <adrianresende147@gmail.com>
2023-09-25 16:30:18 -0300

    UPLOAD

---
+++ server.js
@@ -0,0 +1,49 @@
+import { DatabaseMemory } from './database-memory.js';
+import { fastify } from 'fastify';
+
+const server = fastify();
+
+const database = new DatabaseMemory();
+
+server.post('/videos', (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
+});
+
+server.get('/videos', () => {
+  const videos = database.list();
+
+  return videos;
+});
+
+server.put('/videos/:id', (request, reply) => {
+  const videoId = request.params.id;
+  const { title, description, duration } = request.body;
+
+  database.uptade(videoId, {
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(204).send();
+});
+
+server.delete('/videos/:id', (request, reply) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+
+  return reply.status(204).send();
+});
+
+server.listen({
+  port: 3333,
+});
